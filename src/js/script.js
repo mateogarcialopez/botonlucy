3aa2be1443f75fb2cbb0b7f36f286f36d4eadec0
mateogarcialopez <mateogarcialopez3@gmail>
2020-02-25 15:10:57 -0500

    primer commit

---
+++ src/js/script.js
@@ -0,0 +1,44 @@
+$(function () {
+
+
+  var flag = 0;
+  var flag1 = 0;
+
+  //Funcion para cerrar el chat desde la (X)
+  $('#botonCerrar').on('click', function () {
+    document.getElementById('ifr1').style.display = 'none';
+    flag = 0;
+  });
+
+//Controlar el abrir y cerrar el chat desde el boton
+  $('.share, .contenido').on('click', function () {
+
+    if (flag == 0) {
+      $('.one iframe').delay().fadeIn();
+
+      //---------------cerrar la ventana modal-----------------------------
+      document.getElementById("modal").style.display = "none"; //Reemplazar el ID del modal
+      //---------------cerrar la ventana modal-----------------------------
+      
+      document.getElementById("ifr1").style.display = "block";
+      document.getElementById("botonCerrar").style.display = "block";
+
+
+      document.getElementById("ifr1").style.opacity = "3";
+      flag = 1;
+      flag1 = 1;
+    }
+
+
+    else {
+      $('.one iframe').delay().fadeOut();
+      document.getElementById("ifr1").style.display = "none";
+      
+      flag = 0;
+    }
+  });
+});
+
+
+
+
