af5dcd2a7674be1c1fe5d68d71d9ef8b12d2be94
Mateo Garcia <mateogarcialopez3@gmail.com>
2020-04-03 10:40:55 -0500

    responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,88 @@
 import { opens_chat, dialog_id } from "./config.js";
 
-$(function () {
 
-  var flag = 0;
-  var to;
+if (screen.width <= 768) {
 
-  //Funcion para cerrar el chat desde la (X)
-  $('#botonMin, #cabecera').on('click', function () {
-    document.getElementById('ifr1').style.display = 'none';    
-    document.getElementById('share').style.display = 'block';
-    flag = 0;
-  });
+  $(function () {
 
+    document.getElementById('one').style.right = '-60px';
+    document.getElementById('one').style.bottom = '-40px';
+    var flag = 0;
+    var to;
 
-  //Controlar el abrir y cerrar el chat desde el boton
-  opens_chat.on('click', function () {
-
-    if (flag == 0) {
-      $('.one iframe').delay().fadeIn();
-
-      dialog_id.hide();
-
-      document.getElementById("ifr1").style.display = "block";
-      document.getElementById("botonMin").style.display = "block";
-      document.getElementById('share').style.display = 'none';
-
-
-      document.getElementById("ifr1").style.opacity = "3";
-      flag = 1;
-    } else {
-      $('.one iframe').delay().fadeOut();
-      document.getElementById("ifr1").style.display = "none";
+    //Funcion para cerrar el chat desde la (X)
+    $('#botonMin, #cabecera').on('click', function () {
+      document.getElementById('ifr1').style.display = 'none';
+      document.getElementById('share').style.display = 'block';
       flag = 0;
-      clearTimeout(to);
-    }
+    });
+
+
+    //Controlar el abrir y cerrar el chat desde el boton
+    opens_chat.on('click', function () {
+
+      if (flag == 0) {
+        $('.one iframe').delay().fadeIn();
+
+        dialog_id.hide();
+
+        document.getElementById("ifr1").style.display = "block";
+        document.getElementById("botonMin").style.display = "block";
+        document.getElementById('share').style.display = 'none';
+
+
+        document.getElementById("ifr1").style.opacity = "3";
+        flag = 1;
+      } else {
+        $('.one iframe').delay().fadeOut();
+        document.getElementById("ifr1").style.display = "none";
+        flag = 0;
+        clearTimeout(to);
+      }
+    });
   });
-});
+
+
+} else {
+
+  $(function () {
+
+    var flag = 0;
+    var to;
+
+    //Funcion para cerrar el chat desde la (X)
+    $('#botonMin, #cabecera').on('click', function () {
+      document.getElementById('ifr1').style.display = 'none';
+      flag = 0;
+    });
+
+
+    //Controlar el abrir y cerrar el chat desde el boton
+    opens_chat.on('click', function () {
+
+      if (flag == 0) {
+        $('.one iframe').delay().fadeIn();
+
+        dialog_id.hide();
+
+        document.getElementById("ifr1").style.display = "block";
+        document.getElementById("botonMin").style.display = "block";
+
+
+        document.getElementById("ifr1").style.opacity = "3";
+        flag = 1;
+      } else {
+        $('.one iframe').delay().fadeOut();
+        document.getElementById("ifr1").style.display = "none";
+        flag = 0;
+        clearTimeout(to);
+      }
+    });
+  });
+
+}
+
+
 
 
 
